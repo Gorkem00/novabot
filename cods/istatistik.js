a935411c7304a2e550e8d8188d190c74344f1dab
Gorkem00 <59614699+Gorkem00@users.noreply.github.com>
2020-01-12 17:08:28 +0300

    Create istatistik.js

---
+++ cods/istatistik.js
@@ -0,0 +1,36 @@
+
+Save New Duplicate & Edit Just Text
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const moment = require("moment");
+require("moment-duration-format");
+
+module.exports.run = async(client, message, args) => {
+  const duration = moment.duration(client.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
+
+    const embed = new Discord.RichEmbed()
+    .setColor('BLACK')
+    .setTitle(':bar_chart: İstatistik;')
+    .addField(':timer: Ping : ', client.ping + 'ms')
+    .addField(':construction_worker: Çalışma Süresi: ', `${duration}`)
+    .addField(':busts_in_silhouette: Kullanıcılar:', client.guilds.reduce((a, b) => a + b.memberCount, 0))
+    .addField(':tv: Kanallar:', client.channels.size)
+    .addField(':clipboard: Sunucular:', client.guilds.size)
+    .addField(':desktop: Bellek kullanımı:', (process.memoryUsage().heapUsed / 2048 / 2048).toFixed(2))
+    .addField(':book: Kütüphanesi;', `Discord.js`)
+    .addField(`Discord.js sürümü:`, Discord.version)
+    .setTimestamp()
+    message.channel.send(embed);
+}
+exports.conf = {
+  enabled: true,
+    guildOnly: true,
+  aliases: ['istatistik', 'botbilgi', 'bot-bilgi', 'i'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'bilgi-bot',
+  description: 'İstatistik.',
+  usage: 'İstatistik'
+};
